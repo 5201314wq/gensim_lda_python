23594d1329ff35b78754552ae42e835f4c252737
qian.wu <qian.wu@aispeech.com>
2018-03-06 14:30:23 +0800

    alter file

--- tutorials/topics_and_transformations/transformation_interface.py
+++ tutorials/topics_and_transformations/transformation_interface.py
@@ -18,15 +18,22 @@ doc_bow = [(0, 1), (1, 1)]
 print(tfidf[doc_bow]) # step 2 -- use the model to transform vectors
 corpus_tfidf = tfidf[corpus]
 for doc in corpus_tfidf:
-    print(doc)
+    print("tfidf", doc)
 
 lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=2) # initialize an LSI transformation
 corpus_lsi = lsi[corpus_tfidf] # create a double wrapper over the original corpus: bow->tfidf->fold-in-lsi
 lsi.print_topics(2)
-
 for doc in corpus_lsi: # both bow->tfidf and tfidf->lsi transformations are actually executed here, on the fly
-    print(doc)
-
+    print("lsimodel", doc)
 lsi.save('/tmp/model.lsi') # same for tfidf, lda, ...
 lsi = models.LsiModel.load('/tmp/model.lsi')
+
+lda = models.LdaModel(corpus, id2word=dictionary, num_topics=1)
+corpus_lda = lda[corpus_tfidf]
+lda.print_topic(2)
+lda.save('model.lda')
+corpora.MmCorpus.serialize('lda_corpus.mm', corpus_lda)
+for doc in corpus_lda:
+    print("ldamodel", doc)
+
 
