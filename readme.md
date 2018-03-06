1220dd3d0624bb17c348f9f22bc451346b049e55
qian.wu <qian.wu@aispeech.com>
2018-03-06 14:14:11 +0800

    add new example

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 gensim官网的所有列子
+
+文档链接请点击[这里](https://radimrehurek.com/gensim/tutorial.html)
---
+++ tutorials/topics_and_transformations/transformation_interface.py
@@ -0,0 +1,32 @@
+import os
+from gensim import corpora, models, similarities
+
+## Creating a transformation
+# first you should run the script memory_friendly.py
+if (os.path.exists("/tmp/deerwester.dict")):
+    dictionary = corpora.Dictionary.load('/tmp/deerwester.dict')
+    corpus = corpora.MmCorpus('/tmp/deerwester.mm')
+    print("Used files generated from first tutorial")
+else:
+    print("Please run first tutorial/corpora_and_vector_spaces/memory_friendly\
+          to generate data set")
+
+
+tfidf = models.TfidfModel(corpus) # step 1 --initialize a model
+## transforming vectors
+doc_bow = [(0, 1), (1, 1)]
+print(tfidf[doc_bow]) # step 2 -- use the model to transform vectors
+corpus_tfidf = tfidf[corpus]
+for doc in corpus_tfidf:
+    print(doc)
+
+lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=2) # initialize an LSI transformation
+corpus_lsi = lsi[corpus_tfidf] # create a double wrapper over the original corpus: bow->tfidf->fold-in-lsi
+lsi.print_topics(2)
+
+for doc in corpus_lsi: # both bow->tfidf and tfidf->lsi transformations are actually executed here, on the fly
+    print(doc)
+
+lsi.save('/tmp/model.lsi') # same for tfidf, lda, ...
+lsi = models.LsiModel.load('/tmp/model.lsi')
+
